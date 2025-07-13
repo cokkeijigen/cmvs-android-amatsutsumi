d9373999f5980809bcf82b03060d9ba55476d8ef
cokkeijigen <itsukezigen@gmail.com>
2025-07-13 21:48:55 +0800

    Update native-lib.cpp

--- amatsutsumi/app/src/main/cpp/native-lib.cpp
+++ amatsutsumi/app/src/main/cpp/native-lib.cpp
@@ -10,19 +10,34 @@ static auto call_type mbstowcs_one_hook(uint16_t uChar) -> wchar_t
     return hooker::call<mbstowcs_one_hook>(uChar);
 }
 
+static auto call_type ApiSjisCheck_Hook(uint8_t chr) -> int
+{
+    return static_cast<int>(chr > 0x7F);
+}
+
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
 
     void* handle { ::dlopen("libamatsutsumi.so", RTLD_NOW) };
     if (handle != nullptr)
     {
-        void* target_function_ptr { ::dlsym(handle, "_Z13_mbstowcs_onet")};
-        if(target_function_ptr)
+        void* mbstowcs_onet_ptr { ::dlsym(handle, "_Z13_mbstowcs_onet") };
+        if(mbstowcs_onet_ptr != nullptr)
         {
-            hooker::add_hook<mbstowcs_one_hook>(target_function_ptr);
+            hooker::add_hook<mbstowcs_one_hook>(mbstowcs_onet_ptr);
         }
         else
         {
-            LOGD("target_function_ptr is nullptr");
+            LOGD("mbstowcs_onet_ptr is nullptr");
+        }
+
+        void* ApiSjisCheck_ptr{ ::dlsym(handle, "_Z12ApiSjisCheckh")  };
+        if(ApiSjisCheck_ptr != nullptr)
+        {
+            hooker::add_hook<ApiSjisCheck_Hook>(ApiSjisCheck_ptr);
+        }
+        else
+        {
+            LOGD("ApiSjisCheck_ptr is nullptr");
         }
     }
     else
